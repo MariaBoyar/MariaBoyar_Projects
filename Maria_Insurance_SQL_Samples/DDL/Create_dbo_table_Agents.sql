bcd64609bbfe35f869b6167892190f0e4d011aa1
MariaBoyar <Commits+MariaBoyar@users.noreply.github.com>
2020-03-06 00:02:03 -0500

    Added Compani ID field

--- Maria_Insurance_SQL_Samples/DDL/Create_dbo_table_Agents.sql
+++ Maria_Insurance_SQL_Samples/DDL/Create_dbo_table_Agents.sql
@@ -5,14 +5,15 @@ IF OBJECT_ID(N'dbo.Agents', N'U') IS NULL
 BEGIN
 CREATE TABLE dbo.Agents(
 	Agt_ID int not null identity (1,1) CONSTRAINT [PK_Agents] PRIMARY KEY,
+	Agc_ID  int not null,
 	Agt_Code varchar(50) not null,
-	Agt_Manager_ID bit not null,
+	Agt_Manager_ID bit null,
 	Agt_FirstName varchar(250) not null,
 	Agt_LastName varchar(250) not null,
 	Agt_DoB date not null,
 	Agt_Phone varchar(20) null,
 	Agt_EntryDate datetime
 ) 
-print 'Table dbo.Agents is created' + char (13)
+print 'Table dbo.Agents is created' 
 END
-else Print 'Table dbo.Agents already exists' + char (13)
+else Print 'Table dbo.Agents already exists' 
